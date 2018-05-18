be4a62778d218fa63a71beb68529163043bb28a7
Ziyan Wang <zw301@nyu.edu>
2018-05-17 17:41:08 -0700

    /**

---
+++ leetcode/17. Letter Combinations of a Phone Number.js
@@ -0,0 +1,54 @@
+// Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+//
+// A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+//
+//
+//
+// Example:
+//
+// Input: "23"
+// Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+// Note:
+//
+// Although the above answer is in lexicographical order, your answer could be in any order you want.
+
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(str) {
+    const map = {
+    "0": [],
+    "1": [],
+    "2": ["a", "b", "c"],
+    "3": ["d", "e", "f"],
+    "4": ["g", "h", "i"],
+    "5": ["j", "k", "l"],
+    "6": ["m", "n", "o"],
+    "7": ["p", "q", "r", "s"],
+    "8": ["t", "u", "v"],
+    "9": ["w", "x", "y", "z"]
+  }
+
+  if(str.length === 0) {
+      return [];
+  }
+  if (str.length === 1) {
+    return map[str];
+  }
+
+  let last = str.slice(-1);
+  let prevStr = str.slice(0, str.length - 1);
+
+  let prev = letterCombinations(prevStr);
+
+  let charArr = map[last];
+  let result = []
+  for (var i = 0; i < charArr.length; i++) {
+    for (var j = 0; j < prev.length; j++) {
+      let string = prev[j] + charArr[i];
+      result.push(string)
+    }
+  }
+  return result;
+};

355d631f0188e0b71e0c4956f18220a8223620ff
Ziyan Wang <zw301@nyu.edu>
2018-05-18 22:11:35 -0700

    Permutation II second time

--- leetcode/47. Permutations II.js
+++ leetcode/47. Permutations II.js
@@ -14,7 +14,8 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permuteUnique = function(nums) {
+
+function permuteUnique(nums) {
   const results = [];
   if (nums === null || nums.length === 0) {
     return results;
@@ -23,12 +24,12 @@ var permuteUnique = function(nums) {
   nums.sort(function(a, b) {
     return a - b;
   })
-
   const permutation = [];
   const visited = new Array(nums.length).fill(false);
+
   permuteHelper(nums, permutation, results, visited);
   return results;
-};
+}
 
 function permuteHelper(nums, permutation, results, visited) {
   if (permutation.length === nums.length) {
@@ -36,7 +37,7 @@ function permuteHelper(nums, permutation, results, visited) {
     return;
   }
 
-  for (var i = 0; i < nums.length; i++) {
+  for (let i = 0; i < nums.length; i++) {
     if (visited[i] || (i > 0 && nums[i] === nums[i - 1] && !visited[i - 1])) {
       continue;
     }
@@ -48,4 +49,42 @@ function permuteHelper(nums, permutation, results, visited) {
   }
 }
 
-console.log(permuteUnique([1,1,2]))
+
+
+
+// var permuteUnique = function(nums) {
+//   const results = [];
+//   if (nums === null || nums.length === 0) {
+//     return results;
+//   }
+//
+//   nums.sort(function(a, b) {
+//     return a - b;
+//   })
+//
+//   const permutation = [];
+//   const visited = new Array(nums.length).fill(false);
+//   permuteHelper(nums, permutation, results, visited);
+//   return results;
+// };
+//
+// function permuteHelper(nums, permutation, results, visited) {
+//   if (permutation.length === nums.length) {
+//     results.push(permutation.slice(0));
+//     return;
+//   }
+//
+//   for (var i = 0; i < nums.length; i++) {
+//     if (visited[i] || (i > 0 && nums[i] === nums[i - 1] && !visited[i - 1])) {
+//       continue;
+//     }
+//     permutation.push(nums[i]);
+//     visited[i] = true;
+//     permuteHelper(nums, permutation, results, visited);
+//     visited[i] = false ;
+//     permutation.pop();
+//   }
+// }
+//
+// console.log(permuteUnique([1,1,2]))
+console.log(permuteUnique([1,2,1]))

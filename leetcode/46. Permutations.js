55fb702d3dc50a67227ca02b339389dcca3c7311
Ziyan Wang <zw301@nyu.edu>
2018-05-18 22:04:18 -0700

    Permutation with duplicated number

--- leetcode/46. Permutations.js
+++ leetcode/46. Permutations.js
@@ -17,29 +17,61 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permute = function(nums) {
+
+function permute(nums) {
   const results = [];
   if (nums === null || nums.length === 0) {
     return results;
   }
 
-  if (nums.length === 1) {
-    return [nums];
-  }
-  const last = nums[nums.length - 1];
-
-  const prev = permute(nums.slice(0, nums.length - 1));
-
-
-  for (var i = 0; i < prev.length; i++) {
-    let len = prev[i].length;
-    for (var j = 0; j <= len; j++) {
-      newArr = prev[i].slice(0, j).concat([last]).concat(prev[i].slice(j + 1));
-      results.push(newArr);
-    }
-  }
+  const permutation = [];
+  permuteHelper(nums, permutation, results);
 
   return results;
-};
+}
 
-console.log(permute([1,2,3]));
+function permuteHelper(nums, permutation, results) {
+  if (permutation.length === nums.length) {
+    results.push(permutation.slice(0));
+    return;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (permutation.includes(nums[i])) {
+      continue;
+    }
+    permutation.push(nums[i]);
+    permuteHelper(nums, permutation, results);
+    permutation.pop();
+  }
+}
+
+console.log(permute([1,2,3]))
+
+
+// var permute = function(nums) {
+//   const results = [];
+//   if (nums === null || nums.length === 0) {
+//     return results;
+//   }
+//
+//   if (nums.length === 1) {
+//     return [nums];
+//   }
+//   const last = nums[nums.length - 1];
+//
+//   const prev = permute(nums.slice(0, nums.length - 1));
+//
+//
+//   for (var i = 0; i < prev.length; i++) {
+//     let len = prev[i].length;
+//     for (var j = 0; j <= len; j++) {
+//       newArr = prev[i].slice(0, j).concat([last]).concat(prev[i].slice(j + 1));
+//       results.push(newArr);
+//     }
+//   }
+//
+//   return results;
+// };
+
+// console.log(permute([1,2,3]));
--- leetcode/47. Permutations II.js
+++ leetcode/47. Permutations II.js
@@ -15,5 +15,37 @@
  * @return {number[][]}
  */
 var permuteUnique = function(nums) {
+  const results = [];
+  if (nums === null || nums.length === 0) {
+    return results;
+  }
 
+  nums.sort(function(a, b) {
+    return a - b;
+  })
+
+  const permutation = [];
+  const visited = new Array(nums.length).fill(false);
+  permuteHelper(nums, permutation, results, visited);
+  return results;
 };
+
+function permuteHelper(nums, permutation, results, visited) {
+  if (permutation.length === nums.length) {
+    results.push(permutation.slice(0));
+    return;
+  }
+
+  for (var i = 0; i < nums.length; i++) {
+    if (visited[i] || (i > 0 && nums[i] === nums[i - 1] && !visited[i - 1])) {
+      continue;
+    }
+    permutation.push(nums[i]);
+    visited[i] = true;
+    permuteHelper(nums, permutation, results, visited);
+    visited[i] = false;
+    permutation.pop();
+  }
+}
+
+console.log(permuteUnique([1,1,2]))

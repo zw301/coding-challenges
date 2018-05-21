6633f628c14dff229df71a7437561a5cc20584a0
Ziyan Wang <zw301@nyu.edu>
2018-05-20 22:25:59 -0700

    Move Zeroes

---
+++ leetcode/283. Move Zeroes.js
@@ -0,0 +1,31 @@
+// Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+//
+// Example:
+//
+// Input: [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Note:
+//
+// You must do this in-place without making a copy of the array.
+// Minimize the total number of operations.
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  let j = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[j] = nums[i];
+      j++;
+    }
+  }
+
+  while (j < nums.length) {
+    nums[j] = 0;
+    j++;
+  }
+};
+
+console.log(moveZeroes([0,1,0,3,12]));

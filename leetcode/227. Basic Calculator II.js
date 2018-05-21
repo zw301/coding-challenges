f5116a5e2a8d6f590c9b28e0d3587412948e4804
Ziyan Wang <zw301@nyu.edu>
2018-05-20 21:24:55 -0700

    Basic calculator II

---
+++ leetcode/227. Basic Calculator II.js
@@ -0,0 +1,69 @@
+// Implement a basic calculator to evaluate a simple expression string.
+//
+// The expression string contains only non-negative integers, +, -, *, / operators and empty spaces .
+// The integer division should truncate toward zero.
+//
+// Example 1:
+//
+// Input: "3+2*2"
+// Output: 7
+// Example 2:
+//
+// Input: " 3/2 "
+// Output: 1
+// Example 3:
+//
+// Input: " 3+5 / 2 "
+// Output: 5
+// Note:
+//
+// You may assume that the given expression is always valid.
+// Do not use the eval built-in library function.
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function(s) {
+  if (s === null || s.length === 0) {
+    return 0;
+  }
+
+  const stack = [];
+  let num = 0;
+  let sign = "+";
+  const len = s.length;
+
+  for (let i = 0; i < len; i++) {
+    const c = s[i];
+    if (c >= "0" && c <= "9") {
+      num = num * 10 + parseInt(c);
+    }
+    if (c === "+" || c === "-" || c === "*" || c === "/" || i === len - 1) {
+      if (sign === "+") {
+        stack.push(num);
+      }
+      if (sign === "-") {
+        stack.push(-num);
+      }
+      if (sign === "*") {
+        stack.push(stack.pop() * num);
+      }
+      if (sign === "/") {
+        stack.push(Math.floor(stack.pop() / num));
+      }
+      sign = c;
+      num = 0;
+    }
+  }
+  let res = 0;
+  for (let i = 0; i < stack.length; i++) {
+    res += stack[i]
+  }
+
+  return res;
+};
+
+
+console.log(calculate("3+2*2"));
+console.log(calculate(" 3/2 "));
+console.log(calculate(" 3+5 / 2 "));

c50de10fc8b317b6eda19537c824d5cb010b3449
Ziyan Wang <zw301@nyu.edu>
2018-05-25 16:34:59 -0700

    Factorial Trailing Zeroes

---
+++ leetcode/172. Factorial Trailing Zeroes.js
@@ -0,0 +1,26 @@
+// Given an integer n, return the number of trailing zeroes in n!.
+//
+// Example 1:
+//
+// Input: 3
+// Output: 0
+// Explanation: 3! = 6, no trailing zero.
+// Example 2:
+//
+// Input: 5
+// Output: 1
+// Explanation: 5! = 120, one trailing zero.
+// Note: Your solution should be in logarithmic time complexity.
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function(n) {
+  let result = 0;
+  for (let i = 5; Math.floor(n / i) > 0; i *= 5) {
+    result += Math.floor(n / i);
+  }
+  return result;
+};
+
+console.log(trailingZeroes(26));

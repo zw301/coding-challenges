526449da8d2a23759d5fbe4abb4b0d552d2bba28
Ziyan Wang <zw301@nyu.edu>
2018-06-11 17:34:46 -0700

    49 Group Anagrams

---
+++ leetcode/49. Group Anagrams.js
@@ -0,0 +1,69 @@
+// Given an array of strings, group anagrams together.
+//
+// Example:
+//
+// Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+// Output:
+// [
+//   ["ate","eat","tea"],
+//   ["nat","tan"],
+//   ["bat"]
+// ]
+// Note:
+//
+// All inputs will be in lowercase.
+// The order of your output does not matter.
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const sorted = strs.map(str => {
+      return str.split("").sort(function(x, y) {
+        return x < y ? -1 : 1;
+      }).join("");
+    })
+
+    const result = [];
+    const checked = {};
+    let target;
+
+    for (let i = 0; i < sorted.length; i++) {
+      target = sorted[i];
+      if (checked[target]) {
+        continue;
+      }
+      let curr = [];
+      for (let j = 0; j < strs.length; j++) {
+        if (sorted[j] === target) {
+          curr.push(strs[j]);
+        }
+      }
+      checked[target] = true;
+      result.push(curr);
+    }
+    return result;
+};
+
+var groupAnagrams = function(strs) {
+  const sorted = strs.map(str => {
+    return str.split("").sort(function(x, y) {
+      return x < y ? -1 : 1;
+    }).join("");
+  })
+
+  const map = new Map();
+
+  for (var i = 0; i < sorted.length; i++) {
+    if (map.has(sorted[i])) {
+      map.get(sorted[i]).push(strs[i]);
+    } else {
+      map.set(sorted[i], [strs[i]]);
+    }
+  }
+
+  return Array.from(map.values());
+}
+
+const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+console.log(groupAnagrams(strs));

f30d00a9906f15ff7edf9bab5ab11d5197e5f7a8
Ziyan Wang <zw301@nyu.edu>
2018-06-21 16:50:34 -0700

    131 Palindrome Partitioning

---
+++ leetcode/131. Palindrome Partitioning.js
@@ -0,0 +1,57 @@
+// Given a string s, partition s such that every substring of the partition is a palindrome.
+//
+// Return all possible palindrome partitioning of s.
+//
+// Example:
+//
+// Input: "aab"
+// Output:
+// [
+//   ["aa","b"],
+//   ["a","a","b"]
+// ]
+/**
+ * @param {string} s
+ * @return {string[][]}
+ */
+ var partition = function(s) {
+   if (s === null || s.length === 0) {
+     return [];
+   }
+
+   const partition = [];
+   const result = [];
+   helper(s, 0, partition, result);
+   return result;
+ };
+
+ function helper(s, startIndex, partition, result) {
+   if (startIndex === s.length) {
+     result.push(partition.slice());
+     return;
+   }
+
+   for (let i = startIndex; i < s.length; i++) {
+     let substr = s.slice(startIndex, i + 1);
+     if (!isPalindrome(substr)) {
+       continue;
+     }
+     partition.push(substr);
+     helper(s, i + 1, partition, result);
+     partition.pop();
+   }
+ }
+
+ function isPalindrome(str) {
+   let i = 0;
+   let j = str.length - 1;
+
+   while (i < j) {
+     if (str[i] !== str[j]) {
+       return false;
+     }
+     i++;
+     j--;
+   }
+   return true;
+ }

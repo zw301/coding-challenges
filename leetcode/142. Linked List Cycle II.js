fc622e5240c591e4556eb44f1491c83cb06e62c2
Ziyan Wang <zw301@nyu.edu>
2018-06-29 16:57:18 -0700

    Linked List Cycle II solution2

--- leetcode/142. Linked List Cycle II.js
+++ leetcode/142. Linked List Cycle II.js
@@ -23,7 +23,29 @@ var detectCycle = function(head) {
             return head;
         }
     }
+    return null;
+};
 
-    return null
+var detectCycle = function(head) {
+    if (head === null) {
+        return null;
+    }
 
+    let fast = head.next;
+    let slow = head;
+
+    while (fast !== slow) {
+        if (fast === null || fast.next === null) {
+            return null;
+        }
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+
+    while (head != slow.next) {
+        head = head.next;
+        slow = slow.next;
+    }
+
+    return head;
 };

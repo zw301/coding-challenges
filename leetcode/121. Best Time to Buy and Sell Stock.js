c7d3dd0918aa0a40063fbf5c140fb068ca01ccc9
Ziyan Wang <zw301@nyu.edu>
2018-07-11 21:03:40 -0700

    build your own flux

--- leetcode/121. Best Time to Buy and Sell Stock.js
+++ leetcode/121. Best Time to Buy and Sell Stock.js
@@ -26,7 +26,7 @@ var maxProfit = function(prices) {
 
   let minPrice = prices[0];
   let maxProfit = 0;
-  for (let i = 0; i < prices.length; i++) {
+  for (let i = 1; i < prices.length; i++) {
     let currPrice = prices[i];
     let currProfit = currPrice - minPrice;
 

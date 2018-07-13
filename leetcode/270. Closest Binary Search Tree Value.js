0e87ef859ff21ca8a161759fd5fd56637005a6a9
Ziyan Wang <zw301@nyu.edu>
2018-07-13 09:59:15 -0700

    Closest Binary Search Tree Value

---
+++ leetcode/270. Closest Binary Search Tree Value.js
@@ -0,0 +1,63 @@
+// Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
+//
+// Note:
+//
+// Given target value is a floating point.
+// You are guaranteed to have only one unique value in the BST that is closest to the target.
+// Example:
+//
+// Input: root = [4,2,5,1,3], target = 3.714286
+//
+//     4
+//    / \
+//   2   5
+//  / \
+// 1   3
+//
+// Output: 4
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {number}
+ */
+
+// 边界数值会超时
+var closestValue = function(root, target) {
+  if (root === null) {
+    return null;
+  }
+  let min = root.val;
+  while (root !== null) {
+    if (Math.abs(root.val - target) < Math.abs(min - target)) {
+      min = root.val;
+    }
+    if (root.val > target) {
+      root = root.left;
+    }
+    if (root.val < target) {
+      root = root.right;
+    }
+  }
+  return min;
+}
+
+var closestValue = function(root, target) {
+  if (root === null) {
+    return null;
+  }
+
+  let kid = root.val > target ? root.left : root.right;
+  if (kid === null) {
+    return root.val;
+  }
+  let closest = closestValue(kid, target);
+
+  return Math.abs(root.val - target) < Math.abs(closest - target) ? root.val : closest;
+}
--- lintcode/900. Closest Binary Search Tree Value.js
+++ lintcode/900. Closest Binary Search Tree Value.js
@@ -8,21 +8,35 @@
  * @param target: the given target
  * @return: the value in the BST that is closest to the target
  */
+// const closestValue = function (root, target) {
+//     if (root === null) {
+//         return null;
+//     }
+//
+//     let min = root.val;
+//     while (root !== null) {
+//         if (Math.abs(min - target) > Math.abs(root.val - target)) {
+//             min = root.val;
+//         }
+//         if (root.val > target) {
+//             root = root.left;
+//         } else if (root.val < target) {
+//             root = root.right;
+//         }
+//     }
+//     return min;
+// }
 const closestValue = function (root, target) {
-    if (root === null) {
-        return null;
-    }
+  if (root === null) {
+    return null;
+  }
 
-    let min = root.val;
-    while (root !== null) {
-        if (Math.abs(min - target) > Math.abs(root.val - target)) {
-            min = root.val;
-        }
-        if (root.val > target) {
-            root = root.left;
-        } else if (root.val < target) {
-            root = root.right;
-        }
-    }
-    return min;
+  let kid = target < root.val ? root.left : root.right;
+  if (kid === null) {
+    return root.val;
+  }
+
+  let closest = closestValue(kid, target);
+
+  return Math.abs(root.val - target) < Math.abs(closest - target) ? root.val : closest;
 }

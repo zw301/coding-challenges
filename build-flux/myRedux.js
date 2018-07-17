710df68abf0ab21cd7c196933dd3ff7e4fddab76
Ziyan Wang <zw301@nyu.edu>
2018-07-16 17:01:26 -0700

    my redux

--- build-flux/myRedux.js
+++ build-flux/myRedux.js
@@ -25,7 +25,7 @@ const createStore = (reducer) => {
 
 const combineReducers = (reducers) => {
   return { state={}, action } => {
-    return Object.keys(reducers).reduce((nextState, key) {
+    return Object.keys(reducers).reduce((nextState, key) => {
       nextState[key] = reducers[key](state[key], action);
       return nextState;
     }, {});
---
+++ lintcode/93. Balanced Binary Tree.js
@@ -0,0 +1,41 @@
+// Given a binary tree, determine if it is height-balanced.
+//
+// For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+//
+// Example
+// Given binary tree A = {3,9,20,#,#,15,7}, B = {3,#,20,15,7}
+//
+// A)  3            B)    3
+//    / \                  \
+//   9  20                 20
+//     /  \                / \
+//    15   7              15  7
+// The binary tree A is a height-balanced binary tree, but B is not.
+
+class ResultType {
+  constructor(depth, isBalanced) {
+    this.depth = depth;
+    this.isBalanced = inBalanced;
+  }
+}
+
+const isBalanced = function(root) {
+  return helper(root).isBalanced;
+}
+
+const helper = function(root) {
+  if (root === null) {
+    return new ResultType(0, true);
+  }
+
+  let left = helper(root.left);
+  let right = helper(root.right);
+  if ( !left.isBalanced || !right.isBalanced ) {
+    return new ResultType(-1, false);
+  }
+  if (Math.abs(left.depth - right.depth) > 1) {
+    return new ResultType(-1, false);
+  }
+
+  return new ResultType(Math.max(left.depth, right.depth) + 1, true);
+}

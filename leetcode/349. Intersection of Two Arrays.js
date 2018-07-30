5a6536f18ae098f7d58b50b9286edbdd066cb9cf
Ziyan Wang <zw301@nyu.edu>
2018-07-30 10:49:05 -0700

    349. Intersection of Two Arrays

--- leetcode/349. Intersection of Two Arrays.js
+++ leetcode/349. Intersection of Two Arrays.js
@@ -178,6 +178,36 @@ function bs(arr, target) {
     return false;
 }
 
+// Extra space O(1)
+var intersection = function(nums1, nums2) {
+    if (nums1 === null || nums2 === null) {
+        return null;
+    }
+    nums1.sort((a, b) => a - b);
+    nums2.sort((a, b) => a - b);
+
+    let i = 0;
+    let j = 0;
+    const result = [];
+
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] === nums2[j]) {
+            if (result.length === 0) {
+                result.push(nums1[i]);
+            } else if (result[result.length - 1] !== nums1[i]) {
+                result.push(nums1[i]);
+            }
+            i++;
+            j++;
+        } else if (nums1[i] < nums2[j]) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+    return result;
+};
+
 let nums1 = [1, 2, 2, 1];
 let nums2 = [2, 2]
 

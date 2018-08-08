cadd565958d10f8cb70b70434b1a464e5d579474
Ziyan Wang <zw301@nyu.edu>
2018-08-08 15:26:10 -0700

    139. Word Break

--- leetcode/139. Word Break.js
+++ leetcode/139. Word Break.js
@@ -24,6 +24,11 @@
  * @param {string[]} wordDict
  * @return {boolean}
  */
+
+// We want to know whether s(0~i) can be break, where i = s.length - 1
+// For any given i, if we find a j that s(0~j) can be break, and s(j, i) is in the dictionary
+// We know that s(0~i) can be break
+
 var wordBreak = function(s, wordDict) {
   if (s === null || s.length === 0) {
     return false;
@@ -31,4 +36,36 @@ var wordBreak = function(s, wordDict) {
 
   const canbreak = new Array(s.length + 1).fill(false);
   canbreak[0] = true;
+
+  for (let i = 1; i <= s.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (canbreak[j] && wordDict.includes(s.substring(j, i))) {
+        canbreak[i] = true;
+      }
+    }
+  }
+  return canbreak[s.length];
 };
+
+var wordBreak = function(s, wordDict) {
+  const queue = [];
+  queue.push(0);
+
+  const visited = new Array(s.length).fill(false);
+
+  while (queue.length !== 0) {
+    let start = queue.shift();
+    if (!visited[start]) {
+      for (let i = start + 1; i <= s.length; i++) {
+        if (wordDict.includes(s.substring(start, i))) {
+          if (i === s.length) {
+            return true;
+          }
+          queue.push(i);
+        }
+      }
+      visited[start] = true;
+    }
+  }
+  return false;
+}

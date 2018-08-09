04d83210409704a844d357c111410aae44e063e0
Ziyan Wang <zw301@nyu.edu>
2018-08-09 11:31:05 -0700

    178. Graph Valid Tree & BFS

--- lintcode/178. Graph Valid Tree.js
+++ lintcode/178. Graph Valid Tree.js
@@ -9,39 +9,8 @@
 //
 // Given n = 5 and edges = [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]], return false.
 
-// class UF {
-//   constructor(n){
-//     this.id = new Array(n);
-//     this.numComponent = n;
-//
-//     for (var i = 0; i < n; i++) {
-//       this.id[i] = i;
-//     }
-//   }
-//
-//   find(i) {
-//     while (i !== this.id[i]) {
-//       this.id[i] = this.id[this.id[i]];
-//       i = this.id[i];
-//     }
-//     return i;
-//   }
-//
-//   union(p, q) {
-//     let i = this.find(p);
-//     let j = this.find(q);
-//
-//     if (i !== j) {
-//       this.numComponent--;
-//     }
-//     this.id[i] = j;
-//   }
-//   getComponent() {
-//     return this.numComponent;
-//   }
-// }
 
-
+// Union-Find
 class UF {
   constructor(n) {
     this.id = new Array(n);
@@ -88,6 +57,53 @@ function isValidTree(n, edges) {
   return uf.getComponent() === 1;
 
 }
+
+// BFS
+// If a graph is a valid tree, it satisfies:
+// 1, If and Only if the number of edges equals n - 1
+// 2, All n vertices are connected
+// We can use BFS to traverse the graph and store all reached vertices to a HashSet
+// After traversing, we check if the size of HashSet is n
+
+var validTree = function(n, edges) {
+  if (n === 0) {
+    return false;
+  }
+  if (n - 1 !== edges.length) {
+    return false;
+  }
+
+  const adj = new Array(n);
+  for (let i = 0; i < n; i++) {
+    adj[i] = new Array();
+  }
+
+  for (let i = 0; i < edges.length; i++) {
+    let from = edges[i][0];
+    let to = edges[i][1];
+    adj[from].push(to);
+    adj[to].push(from);
+  }
+
+  const queue = [];
+  const set = new Set();
+
+  queue.push(0);
+  set.add(0);
+
+  while (queue.length !== 0) {
+    let node = queue.shift();
+    for (let i = 0; i < adj[node].length; i++) {
+      if (set.has(adj[node][i])) {
+        continue;
+      }
+      queue.push(adj[node][i]);
+      set.add(adj[node][i]);
+    }
+  }
+
+  return set.size === n;
+};
 
 const n1 = 5;
 const edges1 = [[0, 1], [0, 2], [0, 3], [1, 4]];

c09ac219505873e12bfbbf6be5634058b8828a05
Ziyan Wang <zw301@nyu.edu>
2018-08-11 21:38:36 -0700

    20 solution II

--- leetcode/20. Valid Parentheses.js
+++ leetcode/20. Valid Parentheses.js
@@ -33,6 +33,26 @@
  * @param {string} s
  * @return {boolean}
  */
+
+var isValid = function(s) {
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    if (char === "(") {
+      stack.push(")");
+    } else if (char === "[") {
+      stack.push("]");
+    } else if (char === "{") {
+      stack.push("}");
+    } else if (stack.length !== 0 || stack.pop() !== char) {
+      return false;
+    }
+  }
+  return stack.length === 0;
+}
+
+
 var isValid = function(s) {
   const stack = [];
 

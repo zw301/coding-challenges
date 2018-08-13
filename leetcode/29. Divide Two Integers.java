29db0c13610a173096f23dd75449a60b314b52c0
Ziyan Wang <zw301@nyu.edu>
2018-08-13 09:25:46 -0700

    29. Divide Two Integers.java

--- leetcode/29. Divide Two Integers.java
+++ leetcode/29. Divide Two Integers.java
@@ -25,29 +25,29 @@
  * @return {number}
  */
 
-// var divide = function(dividend, divisor) {
-//    if (divisor === 0) return 0;
-//    if (dividend === 0) return 0;
-//    if (dividend === -2147483648 && divisor === -1) return 2147483647;
-//
-//    let sign = dividend < 0 ^ divisor < 0 ? -1 : 1;
-//
-//    dividend = Math.abs(dividend);
-//    divisor = Math.abs(divisor);
-//
-//    let res = 0;
-//    while (dividend >= divisor) {
-//        let multiple = 1;
-//        let base = divisor;
-//        while (base <= (dividend >> 1)) {
-//            base <<= 1;
-//            multiple <<= 1;
-//        }
-//        res += multiple;
-//        dividend -= base;
-//    }
-//    return sign === -1 ? -res : res;
-// };
+var divide = function(dividend, divisor) {
+   if (divisor === 0) return 0;
+   if (dividend === 0) return 0;
+   if (dividend === -2147483648 && divisor === -1) return 2147483647;
+
+   let sign = dividend < 0 ^ divisor < 0 ? -1 : 1;
+
+   dividend = Math.abs(dividend);
+   divisor = Math.abs(divisor);
+
+   let res = 0;
+   while (dividend >= divisor) {
+       let multiple = 1;
+       let base = divisor;
+       while (base <= (dividend >> 1)) {
+           base <<= 1;
+           multiple <<= 1;
+       }
+       res += multiple;
+       dividend -= base;
+   }
+   return sign === -1 ? -res : res;
+};
 
 
 public class Solution {

3636c41d43b7014b065dba2ad82f3b95de68c9d5
Ziyan Wang <zw301@nyu.edu>
2018-08-13 13:28:34 -0700

    16. 3Sum Closest

---
+++ leetcode/16. 3Sum Closest.js
@@ -0,0 +1,36 @@
+// Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+//
+// Example:
+//
+// Given array nums = [-1, 2, 1, -4], and target = 1.
+//
+// The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+  if (nums === null || nums.length === 0) {
+    return null;
+  }
+
+  nums.sort((a, b) => a - b);
+  let closestSum = nums[0] + nums[1] + nums[2];
+
+  for (let i = 0; i < nums.length; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+      } else if (sum < target){
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+  return closestSum;
+};

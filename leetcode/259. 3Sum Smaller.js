3fd0a3ca3499ddf78fe23c25e012ee983069adb1
Ziyan Wang <zw301@nyu.edu>
2018-08-13 13:49:12 -0700

    259. 3Sum Smaller

---
+++ leetcode/259. 3Sum Smaller.js
@@ -0,0 +1,38 @@
+// Given an array of n integers nums and a target, find the number of index triplets i, j, k with 0 <= i < j < k < n that satisfy the condition nums[i] + nums[j] + nums[k] < target.
+//
+// Example:
+//
+// Input: nums = [-2,0,1,3], and target = 2
+// Output: 2
+// Explanation: Because there are two triplets which sums are less than 2:
+//              [-2,0,1]
+//              [-2,0,3]
+// Follow up: Could you solve it in O(n2) runtime?
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumSmaller = function(nums, target) {
+  if (nums === null || nums.length === 0) {
+    return 0;
+  }
+
+  nums.sort((a, b) => a - b);
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+      if (sum < target) {
+        count += right - left;
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+  return count;
+};

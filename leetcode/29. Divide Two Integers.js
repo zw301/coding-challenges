de02dfce789a86645ad0877d0046418617e3879e
Ziyan Wang <zw301@nyu.edu>
2018-08-13 09:43:52 -0700

    //

--- leetcode/29. Divide Two Integers.js
+++ leetcode/29. Divide Two Integers.js
@@ -24,26 +24,27 @@
  * @param {number} divisor
  * @return {number}
  */
-public class Solution {
-  public int divide(int dividend, int divisor) {
-    if (dividend == 0) {
-      return 0;
-    }
-    if (dividend == Integer.MIN_VALUE && divisor == -1) {
-      return Integer.MAX_VALUE;
-    }
-    int sign = (dividend > 0) ^ (divisor > 0) ? -1 : 1;
-    long a = Math.abs((long) dividend);
-    long b = Math.abs((long) divisor);
-    int result = 0;
-    while (a >= b) {
-      int shift = 0;
-      while (a >= (b << shift)) {
-        shift++;
-      }
-      a -= (b << (shift - 1));
-      result += (1 << (shift - 1));
-    }
-    return sign == 1 ? result : -result;
-  }
-}
+
+var divide = function(dividend, divisor) {
+   if (divisor === 0) return 0;
+   if (dividend === 0) return 0;
+   if (dividend === -2147483648 && divisor === -1) return 2147483647;
+
+   let sign = dividend < 0 ^ divisor < 0 ? -1 : 1;
+
+   dividend = Math.abs(dividend);
+   divisor = Math.abs(divisor);
+
+   let res = 0;
+   while (dividend >= divisor) {
+       let multiple = 1;
+       let base = divisor;
+       while (base <= (dividend >> 1)) {
+           base <<= 1;
+           multiple <<= 1;
+       }
+       res += multiple;
+       dividend -= base;
+   }
+   return sign === -1 ? -res : res;
+};

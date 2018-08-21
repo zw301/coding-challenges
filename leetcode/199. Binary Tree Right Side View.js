eff01569ca5aa503e14b804aaaa0f2b7b28f2ba1
Ziyan Wang <zw301@nyu.edu>
2018-08-21 12:58:16 -0700

    199. Binary Tree Right Side View

---
+++ leetcode/199. Binary Tree Right Side View.js
@@ -0,0 +1,52 @@
+// Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+//
+// Example:
+//
+// Input: [1,2,3,null,5,null,4]
+// Output: [1, 3, 4]
+// Explanation:
+//
+//    1            <---
+//  /   \
+// 2     3         <---
+//  \     \
+//   5     4       <---
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    if (root === null) {
+        return [];
+    }
+
+    const queue = [];
+    queue.push(root);
+
+    const result = [];
+
+    while (queue.length !== 0) {
+        let size = queue.length;
+        for (let i = 0; i < size; i++) {
+            let node = queue.shift();
+            if (i === size - 1) {
+                result.push(node.val);
+            }
+            if (node.left) {
+                queue.push(node.left);
+            }
+            if (node.right) {
+                queue.push(node.right);
+            }
+        }
+    }
+
+    return result;
+};

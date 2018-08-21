d4f59563c34fda516c1872a27b38e7bb0ebd2c27
Ziyan Wang <zw301@nyu.edu>
2018-08-21 14:03:50 -0700

    605. Sequence Reconstruction

--- lintcode/605. Sequence Reconstruction.js
+++ lintcode/605. Sequence Reconstruction.js
@@ -19,6 +19,18 @@
 //
 // Given org = [4,1,5,2,6,3], seqs = [[5,2,6,3],[4,1,5,2]]
 // Return true
+
+// 有且仅有一个拓扑排序。
+// 这道题给了我们一个序列org，又给我们了一些子序列seqs，问这些子序列能否唯一的重建出原序列。
+// 能唯一重建的意思就是任意两个数字的顺序必须是一致的，不能说在一个子序列中1在4的后面，
+// 但是在另一个子序列中1在4的前面，这样就不是唯一的了。还有一点就是，子序列seqs中不能出现其他的数字，
+// 就是说必须都是原序列中的数字。那么我们可以用了一个一维数组pos来记录org中每个数字对应的位置，
+// 然后用一个flags数字来标记当前数字和其前面一个数字是否和org中的顺序一致，
+// 用cnt来标记还需要验证顺序的数字的个数，初始化cnt为n-1，因为n个数字只需要验证n-1对顺序即可，
+// 然后我们先遍历一遍org，将每个数字的位置信息存入pos中，然后再遍历子序列中的每一个数字，
+// 还是要先判断数字是否越界，然后我们取出当前数字cur，和其前一位置上的数字pre，如果在org中，
+// pre在cur之后，那么直接返回false。否则我们看如果cur的顺序没被验证过，而且pre是在cur的前一个，
+// 那么标记cur已验证，且cnt自减1，最后如果cnt为0了，说明所有顺序被成功验证了
 const sequenceReconstruction = function (org, seqs) {
   const map = new Map();
   const indegree = new Map();
@@ -37,18 +49,18 @@ const sequenceReconstruction = function (org, seqs) {
     if (seq.length >= 1 && (seq[0] <= 0 || seq[0] > n)) {
       return false;
     }
-    for (let i = 1; i < seq.length; i++) {
-      console.log(seq[i], n)
-      if (seq[i] <= 0 || seq[i] > n) {
+    for (let j = 1; j < seq.length; j++) {
+      // console.log(seq[i], n)
+      if (seq[j] <= 0 || seq[j] > n) {
         return false;
       }
-      if (map.get(seq[i - 1]) && !map.get(seq[i - 1]).has(seq[i])) {
-        map.get(seq[i - 1]).add(seq[i]);
-        indegree.set(seq[i], indegree.get(seq[i]) + 1);
+      if (map.get(seq[j - 1]) && !map.get(seq[j - 1]).has(seq[j])) {
+        map.get(seq[j - 1]).add(seq[j]);
+        indegree.set(seq[j], indegree.get(seq[j]) + 1);
       }
     }
   }
-  console.log({indegree, map});
+  // console.log({indegree, map});
 
   // case: [1], []
   if (count < n) {

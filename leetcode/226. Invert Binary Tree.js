32da1edb3719e0cb23d96019a4309912387b7713
Ziyan Wang <zw301@nyu.edu>
2018-08-23 17:47:06 -0700

    leetcode/226. Invert Binary Tree

--- leetcode/226. Invert Binary Tree.js
+++ leetcode/226. Invert Binary Tree.js
@@ -38,3 +38,69 @@ var invertTree = function(root) {
   invertTree(root.right);
   return root;
 };
+
+var invertTree = function(root) {
+  if (root === null) {
+    return null;
+  }
+
+  let left = root.left;
+  let right = root.right;
+
+  root.left = invertTree(right);
+  root.right = invertTree(left);
+
+  return root;
+}
+
+// 前序遍历或者后序遍历都可以
+var invertTree = function(root) {
+  if (root === null) {
+    return null;
+  }
+
+  const stack = [];
+  stack.push(root);
+
+  while(stack.length !== 0) {
+    let node = stack.pop();
+    left = node.left;
+    node.left = node.right;
+    node.right = left;
+
+    if (node.left !== null) {
+      stack.push(node.left);
+    }
+    if (node.right !== null) {
+      stack.push(node.right);
+    }
+  }
+
+  return root;
+}
+
+//bfs
+var invertTree = function(root) {
+  if (root === null) {
+    return null;
+  }
+
+  const queue = [];
+  queue.push(root);
+
+  while(queue.length !== 0) {
+    let node = queue.shift();
+    left = node.left;
+    node.left = node.right;
+    node.right = left;
+
+    if (node.left !== null) {
+      queue.push(node.left);
+    }
+    if (node.right !== null) {
+      queue.push(node.right);
+    }
+  }
+
+  return root;
+}

0f972ffb4b75062f272843636f03cbd491479e2d
Ziyan Wang <zw301@nyu.edu>
2018-08-23 17:30:36 -0700

    129. Sum Root to Leaf Numbers

---
+++ leetcode/129. Sum Root to Leaf Numbers.js
@@ -0,0 +1,120 @@
+// Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+//
+// An example is the root-to-leaf path 1->2->3 which represents the number 123.
+//
+// Find the total sum of all root-to-leaf numbers.
+//
+// Note: A leaf is a node with no children.
+//
+// Example:
+//
+// Input: [1,2,3]
+//     1
+//    / \
+//   2   3
+// Output: 25
+// Explanation:
+// The root-to-leaf path 1->2 represents the number 12.
+// The root-to-leaf path 1->3 represents the number 13.
+// Therefore, sum = 12 + 13 = 25.
+// Example 2:
+//
+// Input: [4,9,0,5,1]
+//     4
+//    / \
+//   9   0
+//  / \
+// 5   1
+// Output: 1026
+// Explanation:
+// The root-to-leaf path 4->9->5 represents the number 495.
+// The root-to-leaf path 4->9->1 represents the number 491.
+// The root-to-leaf path 4->0 represents the number 40.
+// Therefore, sum = 495 + 491 + 40 = 1026.
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function(root) {
+    if (root === null) {
+        return 0;
+    }
+
+    const queue = [];
+    queue.push(root);
+
+    const paths = [];
+    paths.push(String(root.val));
+
+    const result = [];
+
+    while (queue.length !== 0) {
+        let size = queue.length;
+        for (let i = 0; i < size; i++) {
+            let node = queue.shift();
+            let p = paths.shift();
+
+            if (node.left === null && node.right === null) {
+                result.push(p);
+            }
+            if (node.left !== null) {
+                queue.push(node.left);
+                paths.push(p + String(node.left.val));
+            }
+
+            if (node.right !== null) {
+                queue.push(node.right);
+                paths.push(p + String(node.right.val));
+            }
+        }
+    }
+
+    let sum = 0;
+    for (let i = 0; i < result.length; i++) {
+        sum += Number(result[i]);
+    }
+
+    return sum;
+};
+
+
+// dfs
+var sumNumbers = function(root) {
+    const paths = getPaths(root);
+
+    let sum = 0;
+    for (let i = 0; i < paths.length; i++) {
+      sum += Number(paths[i]);
+    }
+    return sum;
+};
+
+const getPaths = function(root) {
+  if (root === null) {
+    return [];
+  }
+
+  const result = [];
+  if (root.left === null && root.right === null) {
+    result.push(String(root.val));
+    return result;
+  }
+  let left = getPaths(root.left);
+  let right = getPaths(root.right);
+
+  for (let i = 0; i < left.length; i++) {
+    result.push(String(root.val) + left[i]);
+  }
+  for (let i = 0; i < right.length; i++) {
+    result.push(String(root.val) + right[i]);
+  }
+
+  return result;
+};

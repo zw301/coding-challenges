e0acea2eb764468c10dd18ece13e976bd4fa56a5
Ziyan Wang <zw301@nyu.edu>
2018-08-30 10:44:27 -0700

    79. Word Search

--- leetcode/79. Word Search.js
+++ leetcode/79. Word Search.js
@@ -51,3 +51,48 @@ const helper = function(board, i, j, pos, word) {
 const inBound = function(board, i, j) {
   return i >= 0 && i < board.length && j >= 0 && j < board[0].length;
 };
+
+
+var exist = function(board, word) {
+  if (board === null || board.length === 0 || board[0] === null || board[0].length === 0) {
+    return false;
+  }
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (helper(board, i, j, word, 0)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+const helper = function(board, i, j, word, pos) {
+  if (!inBound(board, i, j) || board[i][j] !== word[pos]) {
+    return false;
+  }
+
+  if (pos === word.length - 1) {
+    return true;
+  }
+  
+  board[i][j] = '#';
+
+  const dirX = [1, -1, 0, 0];
+  const dirY = [0, 0, 1, -1];
+  for (let k = 0; k < 4; k++) {
+    let x = i + dirX[k];
+    let y = j + dirY[k];
+    if (helper(board, x, y, word, pos + 1)) {
+      return true;
+    }
+  }
+
+  board[i][j] = word[pos];
+  return false;
+}
+
+const inBound = function(board, i, j) {
+  return i >= 0 && i < board.length && j >= 0 && j < board[0].length;
+}
